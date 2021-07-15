fcc41b06342a5e4e07d1286f0161fa42a06c106a
olegatorh <herston4ik@gmail.com>
2021-07-16 00:25:34 +0300

    change css

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -5,42 +5,43 @@ import AboutUsSecond from "../media/AboutUsSecond.png"
 import backGround from "../media/background.png"
 
 const useStyles = makeStyles((theme) => ({
-    AboutUsImage: {
-        objectFit: "fill",
-        width: "100%",
-        height: "auto",
-        maxWidth: "450px",
-        maxHeight: "400px",
-        marginRight: "20px",
-        [theme.breakpoints.down("sm")]: {
-            width: "auto",
-            height: "auto",
-            maxWidth: "350px",
-            maxHeight: "350px",
-            objectFit: "fill",
-            margin: "0",
-            flexGrow: "0"
-        }
-
-    },
-    AboutUsMedia: {
-        marginLeft: "20px",
-        [theme.breakpoints.down("sm")]: {
-            marginTop: "10px",
-            marginBottom: "20px",
-            marginLeft: "0px",
-
-        }
-    },
-    GreenLine: {
-        backgroundColor: "#73a640",
-        borderRadius: "0px 20px 20px 0px",
-        color: "white",
-    },
     AboutUs: {
         marginTop: "3%",
         marginBottom: "3%",
         backgroundImage: `url(${backGround})`,
+    },
+    AboutUsImage: {
+        objectFit: "cover",
+        width: "100%",
+        height: "auto",
+        maxWidth: "450px",
+        maxHeight: "450px",
+        [theme.breakpoints.down("sm")]: {
+            marginTop: "10px",
+            marginBottom: "20px",
+            maxWidth: "300px",
+            maxHeight: "250px",
+        }
+
+    },
+    Text: {
+        margin: "20px"
+    },
+    GreenText: {
+        backgroundColor: "#73a640",
+        borderRadius: "0px 20px 20px 0px",
+        color: "white",
+
+    },
+    Line: {
+        border: "1px solid #73a640",
+        borderRadius: "20px",
+        maxWidth: "90%",
+        opacity: "0.5",
+        [theme.breakpoints.down("xs")]: {
+            marginTop: "30px"
+        }
+
     }
 }))
 
@@ -48,75 +49,56 @@ const useStyles = makeStyles((theme) => ({
 export default function AboutUs() {
     const classes = useStyles()
     const theme = useTheme()
-    const matches = useMediaQuery(theme.breakpoints.down("xs"))
+    const matches = useMediaQuery(theme.breakpoints.down("sm"))
 
     return (
         <Grid container className={classes.AboutUs}>
 
-            <Grid container className={classes.AboutUsMedia} direction={matches ? "column" : "row"} justifyContent="center"
+            <Grid container direction={matches ? "column" : "row"}
+                  justifyContent="center"
                   alignItems="center">
-                <Grid item xs style={{
-                    display: "flex",
-                    alignItems: "center",
-                    justifyContent: "center"
-                }}>
-                    <p className={classes.AboutUsMedia}>
+                <Grid item xs>
+                    <Typography align={"center"} className={classes.Text} variant={"h6"}>
                         <strong>«РАНЕВИЦЬКІ КОВБАСИ»</strong> - це з десяток великих,<br/>
                         середніх та малих цехів у яких виробляється майже 20%<br/>
                         від загального обсягу промислової продукції регіону. <br/>
                         За останні два роки, у зв'язку з попитом, ми в декілька разів<br/>
                         збільшили обсяги виробництва продукції.
-                    </p>
+                    </Typography>
                 </Grid>
-                <Grid item xs style={{
-                    display: "flex",
-                    justifyContent: "flex-end",
-                }}>
-
-                    <img className={classes.AboutUsImage} src={AboutUsFirst} alt={"another food"}/>
-
-                </Grid>
-            </Grid>
-
-            <Grid container>
-                <Grid item xs className={classes.GreenLine} style={{
-                    display: "flex",
-                    alignItems: "center",
-                    justifyContent: "center",
-                }}> <Typography align={"center"} variant={"h6"}><strong>МИ ГОРДІ СВОЇМИ
-                    ЗДОБУТКАМИ!</strong></Typography></Grid>
                 <Grid item xs>
-                    <hr style={{
-                        border: "1px solid #73a640",
-                        borderRadius: "20px",
-                        maxWidth: "90%",
-                        opacity: "0.5",
-                    }}/>
+                    <Typography align={"center"} >
+                        <img className={classes.AboutUsImage} src={AboutUsFirst} alt={"another food"}/>
+                    </Typography>
+                </Grid>
+            </Grid>
+            <Grid container>
+                <Grid item xs className={classes.GreenText}>
+                    <Typography align={"center"} variant={"h6"}>
+                        <strong>МИ ГОРДІ СВОЇМИ ЗДОБУТКАМИ!</strong>
+                    </Typography>
+                </Grid>
+                <Grid item xs>
+                    <Typography align={"center"}>
+                        <hr className={classes.Line}/>
+                    </Typography>
                 </Grid>
             </Grid>
 
-            <Grid container  direction={matches ? "column" : "row"}  justifyContent="center" alignItems="center">
-                <Grid item xs style={{
-                    display: "flex",
-                    alignItems: "center",
-                    justifyContent: "center",
-
-                }}>
-                    <p className={classes.AboutUsMedia}>
+            <Grid container direction={matches ? "column" : "row"} justifyContent="center" alignItems="center">
+                <Grid item xs>
+                    <Typography align={"center"} className={classes.Text} variant={"h6"}>
                         <strong>Враховуючи усі потреби покупців,</strong><br/>
                         технологи відродили забуті клаcичні старовинні рецепти<br/>
                         виготовлення ковбасних виробів.<br/>
                         Нам вдалося створити неперевершений смак <br/>
                         об'єднавши традиційні та новітні технології.
-                    </p>
+                    </Typography>
                 </Grid>
-                <Grid item xs style={{
-                    display: "flex",
-                    justifyContent: "flex-end",
-                }}>
-
-                    <img className={classes.AboutUsImage} src={AboutUsSecond} alt={"food"}/>
-
+                <Grid item xs>
+                    <Typography align={"center"}>
+                        <img className={classes.AboutUsImage} src={AboutUsSecond} alt={"food"}/>
+                    </Typography>
                 </Grid>
             </Grid>
         </Grid>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,8 +9,25 @@ import mailBox from "../media/mailbox.png"
 
 const useStyles = makeStyles((theme) => ({
     footer: {
-        maxWidth: "100%",
-        height: "auto",
+        color: "white",
+        backgroundColor: "black"
+    },
+    GreenText: {
+        backgroundColor: "#73a640",
+        padding: "3px",
+    },
+    MainBlackText: {
+        padding: "20px",
+        [theme.breakpoints.down("xs")]: {
+            marginBottom: "10px",
+        }
+    },
+    SecondBlackText: {
+        padding: "20px",
+        borderLeft: "2px solid white",
+        [theme.breakpoints.down("xs")]: {
+            borderLeft: "none",
+        }
     },
     FooterImage: {
         objectFit: "fill",
@@ -18,103 +35,90 @@ const useStyles = makeStyles((theme) => ({
         height: "auto",
         maxHeight: "400px"
     },
-    img: {
-        objectFit: "fill",
-        width: "auto",
-        height: "auto",
-
+    FooterIcons: {
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+        "& img": {
+            padding: "10px"
+        },
+        [theme.breakpoints.down("xs")]: {
+            justifyContent: "space-around",
+        }
     },
-    footerIcons: {
-        padding: "2%",
-        backgroundColor: "black",
-
-    },
-
-
-
 }))
 
 function Footer() {
+
     const classes = useStyles()
     const theme = useTheme()
     const matches = useMediaQuery(theme.breakpoints.down("xs"))
+
     return (
-        <Grid container direction={"column"} style={{color: "white",}}>
+        <Grid container direction={"column"} className={classes.footer}>
+
             <Grid container>
-                <Grid item xs style={{
-                    backgroundColor: "#73a640",
-                    padding: "3px",
-                }}>
+                <Grid item xs className={classes.GreenText}>
                     <Typography align={"center"} variant={"h6"}>
                         <strong> НАША ПРОДУКЦІЯ СМАЧНА, АПЕТИТНА І ЦІЛКОМ БЕЗПЕЧНА ДЛЯ СПОЖИВАННЯ!</strong>
                     </Typography>
                 </Grid>
-                <Grid container style={{padding: "20px", backgroundColor: "black"}}  direction={matches ? "column" : "row"}
-                      justifyContent="center" alignItems="center">
-                    <Grid item xs>
+                <Grid container direction={matches ? "column" : "row"} justifyContent="center" alignItems="center">
+                    <Grid item xs className={classes.MainBlackText}>
                         <Typography align={"center"} variant="h4">
                             МИ ЗНАХОДИМОСЬ
                         </Typography>
                     </Grid>
-                    <Grid item xs style={{
-                        borderLeft: "1px solid white",
-                    }}>
+                    <Grid item xs className={classes.SecondBlackText}>
                         <Typography align={"center"}>
                             У підніжжі прикарпатських гір та річок,<br/>
                             неподалік курортного м. Трускавець та поруч із м. Другобич,<br/>
                             знаходиться тихе, чарівне село Раневичі,<br/>
                             в якому і функціонує наше підприємство «РАНЕВИЦЬКІ КОВБАСИ»<br/>
                         </Typography>
-
                     </Grid>
                 </Grid>
             </Grid>
+
             <Grid item xs>
                 <img src={footer} alt="" className={classes.FooterImage}/>
             </Grid>
-            <Grid container
-                  direction={matches ? "column" : "row"}
-                  justifyContent="center"
-                  alignItems="center"
-                  className={classes.footerIcons}
-            >
-                <Grid container xs direction={"row"} justifyContent="space-around" alignItems="center">
-                    <Grid item xs={4} className={classes.GridIcons}>
-                            <a target="_blank" title="Натисніть щоб знайти на карті!"
-                               href="https://www.google.com/maps?ll=49.300699,23.433007&z=16&t=m&hl=uk&gl=UA&mapclient=embed&q=%D0%B2%D1%83%D0%BB%D0%B8%D1%86%D1%8F+%D0%94%D1%80%D0%BE%D0%B3%D0%BE%D0%B1%D0%B8%D1%86%D1%8C%D0%BA%D0%B0,+52+%D0%91%D0%BE%D1%80%D0%B8%D1%81%D0%BB%D0%B0%D0%B2+%D0%9B%D1%8C%D0%B2%D1%96%D0%B2%D1%81%D1%8C%D0%BA%D0%B0+%D0%BE%D0%B1%D0%BB%D0%B0%D1%81%D1%82%D1%8C+82300"
-                               rel="noreferrer">
-                                <img src={home} alt="" className={classes.img}/>
-                            </a>
-                    </Grid>
-                    <Grid item xs={6}>
-                            УКРАЇНА Львівська область с. Раневичі, вул. Дрогобицька 52.
-                    </Grid>
-                </Grid>
-                <Grid container xs direction={"row"} justifyContent="center" className={classes.footerIcons}
-                      alignItems="center" style={{borderLeft: "1px solid white", borderRight: "1px solid white"}}>
-                    <Grid item xs className={classes.GridIcons}>
-                            <img style={{cursor: "pointer"}} src={phone} alt="" className={classes.img}/>
-                    </Grid>
-                    <Grid container xs direction={"column"} justifyContent="center"
-                          alignItems="center">
-                        <Grid item xs>
-                            067 344 41 81
-                        </Grid>
-                        <Grid item xs>
-                            067 460 25 54
-                        </Grid>
-                    </Grid>
-                </Grid>
-                <Grid container xs direction={"row"} className={classes.footerIcons} justifyContent="space-around"
-                      alignItems="center">
-                    <Grid item xs className={classes.GridIcons}>
-                        <a href="mailto: ran_kach1@ukr.net" title={"натисніть щоб надіслати лист!"}>
-                            <img src={mailBox} alt="" className={classes.img}/>
+
+
+            <Grid container direction={matches ? "column" : "row"}>
+                <Grid item xs className={classes.FooterIcons}>
+                    <Typography align={"left"}>
+                        <a target="_blank" title="Натисніть щоб знайти на карті!"
+                           href="https://www.google.com/maps?ll=49.300699,23.433007&z=16&t=m&hl=uk&gl=UA&mapclient=embed&q=%D0%B2%D1%83%D0%BB%D0%B8%D1%86%D1%8F+%D0%94%D1%80%D0%BE%D0%B3%D0%BE%D0%B1%D0%B8%D1%86%D1%8C%D0%BA%D0%B0,+52+%D0%91%D0%BE%D1%80%D0%B8%D1%81%D0%BB%D0%B0%D0%B2+%D0%9B%D1%8C%D0%B2%D1%96%D0%B2%D1%81%D1%8C%D0%BA%D0%B0+%D0%BE%D0%B1%D0%BB%D0%B0%D1%81%D1%82%D1%8C+82300"
+                           rel="noreferrer">
+                            <img src={home} alt=""/>
                         </a>
-                    </Grid>
-                    <Grid item xs>
+                    </Typography>
+                    <Typography align={"right"}>
+                        УКРАЇНА Львівська область<br/>
+                        с. Раневичі, вул. Дрогобицька 52.
+                    </Typography>
+                </Grid>
+                <Grid item xs className={classes.FooterIcons}
+                      style={{borderLeft: "2px solid white", borderRight: "2px solid white"}}>
+                    <Typography align={"left"}>
+                        <img style={{cursor: "pointer"}} src={phone} alt=""/>
+                    </Typography>
+                    <Typography align={"center"}>
+                        067 344 41 81<br/>
+                        067 460 25 54
+                    </Typography>
+                </Grid>
+                <Grid item xs className={classes.FooterIcons}>
+                    <Typography align={"left"}>
+                        <a href="mailto: ran_kach1@ukr.net" title={"натисніть щоб надіслати лист!"}>
+                            <img src={mailBox} alt=""/>
+                        </a>
+                    </Typography>
+                    <Typography align={"right"}>
                         ran_kach1@ukr.net
-                    </Grid>
+                    </Typography>
                 </Grid>
             </Grid>
 
@@ -124,6 +128,7 @@ function Footer() {
                         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2601.71728322973!2d23.43081801569005!3d49.30069847933321!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x473a4a369e2e3901%3A0x49cf3f4d6b4def74!2z0LLRg9C70LjRhtGPINCU0YDQvtCz0L7QsdC40YbRjNC60LAsIDUyLCDQkdC-0YDQuNGB0LvQsNCyLCDQm9GM0LLRltCy0YHRjNC60LAg0L7QsdC70LDRgdGC0YwsIDgyMzAw!5e0!3m2!1suk!2sua!4v1626203332716!5m2!1suk!2sua"
                         width="100%" height="400" allowFullScreen="" loading="lazy"/>
             </Grid>
+
             <Grid item xs style={{
                 backgroundColor: "#73a640",
             }}>
@@ -131,6 +136,7 @@ function Footer() {
                     <strong>НАШЕ РОЗТАШУВАННЯ НА КАРТАХ!</strong>
                 </Typography>
             </Grid>
+
         </Grid>
 
     )
